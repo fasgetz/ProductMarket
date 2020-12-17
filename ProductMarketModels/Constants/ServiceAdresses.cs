9fb445cf7426f290bcc0325d0dcddf409c22b9ff
Andrew Andrew <fasgetz@hotmail.com>
2020-12-18 02:09:07 +0300

    Init

---
+++ ProductMarketModels/Constants/ServiceAdresses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductMarketModels.Constants
+{
+
+    /// <summary>
+    /// Константы адресов сервисов
+    /// </summary>
+    public static class ServiceAdresses
+    {
+
+        /// <summary>
+        /// Адрес сервиса эластика
+        /// </summary>
+        public const string ElasticSearchServive = "http://192.168.1.66:9500/";
+    }
+}
